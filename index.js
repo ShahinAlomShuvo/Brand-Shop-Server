f71a845dceb8584a3d71f304433afc3f9b2855a6
Shahin Alom <shahinalomshuvo94@gmail.com>
2023-10-23 12:44:13 +0600

    add code for get brand and product

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,10 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// ShopNThread
-// gxgOWEObVPu0e5ag
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4ataypz.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,6 +24,33 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productCollection = client.db("brandShopDB").collection("product");
+    const brandCollection = client.db("brandShopDB").collection("brand");
+
+    // for get brands data
+    app.get("/brands", async (req, res) => {
+      const cursor = brandCollection.find();
+      const result = await cursor.toArray();
+      res.json(result);
+    });
+    // for  product collection
+
+    app.get("/products", async (req, res) => {
+      const brand_name = req.query.brand_name;
+
+      const filter = { brand_name };
+      const cursor = productCollection.find(filter);
+      const result = await cursor.toArray();
+      res.json(result);
+    });
+
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      const result = await productCollection.insertOne(product);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
